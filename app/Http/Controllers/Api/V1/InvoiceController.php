5222f1f1e66432f9cbdca0d88ff77fd8da1ec7f7
Victor Rodas <victormatiasrodas100@gmail.com>
2023-05-05 18:59:21 -0400

    add query handler for index method

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -8,15 +8,34 @@ use App\Http\Requests\UpdateInvoiceRequest;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\InvoiceResource;
+use Illuminate\Http\Request;
+use App\Services\V1\InvoiceQuery;
 
 class InvoiceController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return InvoiceResource::collection(Invoice::paginate());
+        $query = InvoiceQuery::get_user_query($request, InvoiceQuery::EXPECTED_PARAMS);
+        
+        if (count($query) === 0) {
+            return InvoiceResource::collection(Invoice::paginate());
+        }
+
+        if (array_key_exists(InvoiceQuery::ERROR, $query)) {
+            return response($query, 400);
+        }
+
+        $results = (new InvoiceQuery)->get_query_result($query);
+
+        /* No results for valid query */
+        if (array_key_exists(InvoiceQuery::NO_DATA, $results)) {
+            return response($results, 404);
+        }
+
+        return InvoiceResource::collection(InvoiceQuery::paginate($results));
     }
 
     /**
