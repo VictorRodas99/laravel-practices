f19a21cc4d18a44f0c829b32d1912d4b0020737d
Victor Rodas <victormatiasrodas100@gmail.com>
2023-05-05 14:00:18 -0400

    add models and migrations

---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Customer;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
+ */
+class InvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $status = fake()->randomElement(['B', 'P', 'V']);
+        $paid_date = $status == 'P'
+            ? fake()->dateTimeThisDecade()
+            : NULL;
+
+        return [
+            "customer_id" => Customer::factory(),
+            "amount" => fake()->numberBetween(100, 20_000),
+            "status" => $status,
+            "billed_date" => fake()->dateTimeThisDecade(),
+            "paid_date" => $paid_date
+        ];
+    }
+}
---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Customer;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void {
+        Customer::factory()
+            ->count(25)
+            ->hasInvoice(10)
+            ->create();
+        
+        Customer::factory()
+            ->count(100)
+            ->hasInvoice(5)
+            ->create();
+
+        Customer::factory()
+            ->count(50)
+            ->hasInvoice(3)
+            ->create();
+
+        Customer::factory()
+            ->count(5)
+            ->create();
+    }
+}
