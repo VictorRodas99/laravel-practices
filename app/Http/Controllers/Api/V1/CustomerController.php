0586ea9cd422048dfaf201abe31bcb47932d2d62
Victor Rodas <victormatiasrodas100@gmail.com>
2023-05-08 11:19:36 -0400

    change behavior of methods of the Query class + add update function

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Customer;
 use App\Http\Requests\V1\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
@@ -18,7 +18,9 @@ class CustomerController extends Controller
      */
     public function index(Request $request)
     {
-        $query = (new CustomerQuery)->get_user_query($request, CustomerQuery::EXPECTED_PARAMS);
+        $query_handler = new CustomerQuery();
+
+        $query = $query_handler->get_user_query($request, CustomerQuery::EXPECTED_PARAMS);
         $include_invoices = $request->query('includeInvoices');
 
         if (count($query) === 0) {
@@ -32,7 +34,7 @@ class CustomerController extends Controller
             return response($query, 400);
         }
 
-        $results = (new CustomerQuery)->get_query_result($query);
+        $results = $query_handler->get_query_result($query);
 
         /* No results for valid query */
         if (array_key_exists(CustomerQuery::NO_DATA, $results->toArray())) {
@@ -43,7 +45,7 @@ class CustomerController extends Controller
             $results = $results->with('invoices');
         }
 
-        return new CustomerCollection(CustomerQuery::paginate($results));
+        return new CustomerCollection($query_handler->paginate($results));
     }
 
     /**
@@ -73,7 +75,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
