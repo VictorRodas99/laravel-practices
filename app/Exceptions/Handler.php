02220fdbf83c3cadf23ea152901bf2b9d9ecef5d
Victor Rodas <victormatiasrodas100@gmail.com>
2023-05-08 10:53:21 -0400

    add error handling for route not found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,6 +25,32 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
+        $this->renderable(function (NotFoundHttpException $e, Request $request) {
+
+            if ($request->is('api/v1/invoices/*')) {
+                $invoice_id = $request->route('invoice');
+
+                return response()->json([
+                    'message' => "Invoice record {$invoice_id} not found!"
+                ], 404);
+            }
+
+            if ($request->is('api/v1/customers/*')) {
+                $customer_id = $request->route('customer');
+
+                return response()->json([
+                    'message' => "Customer record {$customer_id} not found!"
+                ], 404);
+            }
+
+            // Route not found exception
+            $final_url = $request->path();
+
+            return response()->json([
+                'message' => "The route {$final_url} could not be found"
+            ], 404);
+        });
+
         $this->reportable(function (Throwable $e) {
             //
         });
